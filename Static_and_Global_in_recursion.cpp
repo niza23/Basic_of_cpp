7c50786f42d4a7c849c4a8db40cd44b641ef4e20
niza23 <79857995+niza23@users.noreply.github.com>
2022-02-08 21:05:07 +0530

    Add files via upload

---
+++ Static_and_Global_in_recursion.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+int fun1(int n)
+{
+    if(n>0)
+    {
+        
+        return fun1(n-1)+n; //multiple copies
+    }
+    return 0;
+}
+
+int fun2(int n)
+{
+    static int x=0; //static variable will have only one copy
+    if(n>0)
+    {
+        x++;
+        return fun2(n-1)+x;
+        
+    }
+    return 0;
+}
+
+int y=0;  //global variable
+int fun3(int n)
+{
+    if(n>0)
+    {
+        y++;
+        return fun3(n-1)+y;
+        
+    }
+    return 0;
+}
+
+int main()
+{
+    int r,s,t;
+    s=fun1(5);
+    r=fun2(5);
+    t=fun3(5);
+    cout<<s<<" "<<endl;
+    cout<<r<<" "<<endl;
+    cout<<t;
+    return 0;
+}
