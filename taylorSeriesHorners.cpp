ca17d13e6238f700ae478566e5482bd3dd4bf2f6
niza23 <79857995+niza23@users.noreply.github.com>
2022-02-10 14:54:01 +0530

    Add files via upload

---
+++ taylorSeriesHorners.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+double t2(int x, int n)
+{
+    static double s;
+
+    if(n==0)
+    {
+        return s;
+    }
+    else{
+        s=1+x*s/n;  //type cast to get double
+        
+    }
+    return t2(x,n-1);
+}
+
+int main()
+{
+    //  int r;
+    //  cout<<"hey";
+    // int x=1,m=10;
+    cout<<t2(1,10);
+    return 0;
+}
