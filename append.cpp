1a3b2ec1c335f7ee92378bb6a8d89603f15a5122
niza23 <79857995+niza23@users.noreply.github.com>
2022-02-12 16:25:29 +0530

    Add files via upload

---
+++ append.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+struct array
+{
+    int a[20];
+    int size;
+    int len;
+};
+void display(struct array arr)
+{
+   for(int i=0;i<arr.len;i++)
+    {
+        cout<<arr.a[i]<<endl;
+    } 
+}
+void insert(struct array *arr,int index, int x)
+{
+    if(index>=0 && index<=arr->len)
+    {
+        for(int i=arr->len;i>index;i--)
+        {
+            arr->a[i]=arr->a[i-1];
+        
+        }
+        arr->a[index]=x;
+        arr->len++;
+    }
+
+}
+void append(struct array *arr,int x)  //since we want to modify the array we should use call by address
+{
+    if(arr->len<arr->size)  //check for free space
+    {
+        arr->a[arr->len++]=x;
+    }
+}
+
+int main()
+{
+    struct array arr={{2,3,4,5,5},10,5};
+    append(&arr,20);
+
+    insert(&arr,5,10);
+    
+    display(arr);
+}
---
+++ array_insertion.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+struct array
+{
+    int a[20];
+    int size;
+    int len;
+};
+void display(struct array arr)
+{
+   for(int i=0;i<arr.len;i++)
+    {
+        cout<<arr.a[i]<<endl;
+    } 
+}
+
+int main()
+{
+    struct array arr;
+    int in,x;
+    cout<<"enter index "<<endl;
+    cin>>in;
+    cout<<"enter number "<<endl;
+    cin>>x;
+    cout<<"insert in array "<<endl;
+    cout<<"enter number of element "<<endl;
+    cin>>arr.len;
+    if(arr.len<in)
+    {
+                 cout<<"invLID POsition ";
+    }
+    else
+    {
+        cout<<"enter elements "<<endl;
+         for(int i=0;i<arr.len;i++)
+            {
+                 cin>>arr.a[i];
+             }
+
+         for(int i=arr.len;i>=in;i--)
+            {
+                arr.a[i]=arr.a[i-1];
+             }
+         arr.a[in-1]=x;
+         arr.len++;
+
+         cout<<"new array ";
+         display(arr);
+    }
+         
+    
+}
