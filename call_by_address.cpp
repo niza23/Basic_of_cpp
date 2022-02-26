d0f20cad4d219caf3c2150bfb5bfe3533188c4b0
Nidhi Zala <nidhizala2307@gmail.com>
2022-02-26 23:10:49 +0530

    Update call_by_address.cpp

--- call_by_address.cpp
+++ call_by_address.cpp
@@ -11,7 +11,7 @@ int main()
     int x,y,z;
     x=10;
     y=100;
-    z=add(&x,&y);  //it will pass the address
+    z=add(&x,&y);  //it will pass the address to add function
     cout<<z;
 
     return 0;
