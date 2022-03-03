2a1856e5c54f6587ac1fccc27b5f776fa023234a
Nidhi Zala <nidhizala2307@gmail.com>
2022-03-04 00:12:48 +0530

    Update IncreaseSizeOfArray.cpp

--- IncreaseSizeOfArray.cpp
+++ IncreaseSizeOfArray.cpp
@@ -5,7 +5,7 @@ int main()
 {
     int *p, *q;
     p=new int[5];
-    q=new int[10];  //store the value of p inside it
+    q=new int[10];  //store the value of p inside this new pointer
     p[0]=10;
     p[1]=11;
     p[2]=111;
