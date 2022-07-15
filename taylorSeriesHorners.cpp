e46cfc93c8be755b21079cf0e26aebf61b25de3e
Nidhi Zala <nidhizala2307@gmail.com>
2022-07-15 20:57:32 +0530

    Update taylorSeriesHorners.cpp

--- taylorSeriesHorners.cpp
+++ taylorSeriesHorners.cpp
@@ -6,13 +6,11 @@ double t2(int x, int n)
 {
     static double s;
 
-    if(n==0)
-    {
+    if(n==0){
         return s;
     }
     else{
-        s=1+x*s/n;  //type cast to get double
-        
+        s=1+x*s/n;  //type cast to get double    
     }
     return t2(x,n-1);
 }
@@ -22,6 +20,7 @@ int main()
     //  int r;
     //  cout<<"hey";
     // int x=1,m=10;
+    
     cout<<t2(1,10);
     return 0;
 }
