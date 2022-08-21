f2424297d49111c4f95dd09e35450ac7220a3b3a
Nidhi Zala <nidhizala2307@gmail.com>
2022-08-21 11:12:09 +0530

    Update Static_and_Global_in_recursion.cpp

--- Static_and_Global_in_recursion.cpp
+++ Static_and_Global_in_recursion.cpp
@@ -4,8 +4,7 @@ using namespace std;
 int fun1(int n)
 {
     if(n>0)
-    {
-        
+    {  
         return fun1(n-1)+n; //it will have multiple copies
     }
     return 0;
@@ -15,10 +14,8 @@ int fun2(int n)
 {
     static int x=0; //static variable will have only one copy
     if(n>0)
-    {
-        x++;
-        return fun2(n-1)+x;
-        
+    {   x++;
+        return fun2(n-1)+x;   
     }
     return 0;
 }
